34fb9bd71da1f8616ce38265e68edcfbd1c01b88
MatTz <matheusvenas420@gmail.com>
2026-05-23 11:46:24 -0300

    Tela Login

---
+++ app/src/main/java/com/example/financas/CadastroActivity.java
@@ -0,0 +1,163 @@
+package com.example.financas;
+
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    EditText nome, email, senha, confirmarSenha;
+    Button cadastrar;
+
+    FirebaseAuth auth;
+    FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar);
+
+        nome = findViewById(R.id.edtNome);
+        email = findViewById(R.id.edtEmail);
+        senha = findViewById(R.id.edtSenha);
+        confirmarSenha = findViewById(R.id.edtConfirmarSenha);
+
+        cadastrar = findViewById(R.id.btnCadastrar);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+    }
+
+    public void cadastrar(View v){
+
+        String txtNome = nome.getText().toString();
+        String txtEmail = email.getText().toString();
+        String txtSenha = senha.getText().toString();
+        String txtConfirmarSenha = confirmarSenha.getText().toString();
+
+        if(txtNome.isEmpty() ||
+                txtEmail.isEmpty() ||
+                txtSenha.isEmpty() ||
+                txtConfirmarSenha.isEmpty()){
+
+            Snackbar snac = Snackbar.make(
+                    v,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_SHORT
+            );
+
+            snac.setBackgroundTint(Color.BLACK);
+            snac.setTextColor(Color.WHITE);
+            snac.show();
+
+        } else if(!txtSenha.equals(txtConfirmarSenha)){
+
+            Snackbar snac = Snackbar.make(
+                    v,
+                    "As senhas são diferentes",
+                    Snackbar.LENGTH_SHORT
+            );
+
+            snac.setBackgroundTint(Color.BLACK);
+            snac.setTextColor(Color.WHITE);
+            snac.show();
+
+        } else {
+
+            cadastrarUsuario(v);
+
+        }
+
+    }
+
+    private void cadastrarUsuario(View v){
+
+        String txtNome = nome.getText().toString();
+        String txtEmail = email.getText().toString();
+        String txtSenha = senha.getText().toString();
+
+        auth.createUserWithEmailAndPassword(txtEmail, txtSenha)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            String idUsuario = auth
+                                    .getCurrentUser()
+                                    .getUid();
+
+                            Map<String, Object> usuario = new HashMap<>();
+
+                            usuario.put("nome", txtNome);
+                            usuario.put("email", txtEmail);
+
+                            db.collection("usuarios")
+                                    .document(idUsuario)
+                                    .set(usuario);
+
+                            Intent intent = new Intent(
+                                    CadastroActivity.this,
+                                    MainActivity.class
+                            );
+
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+
+                            String erro;
+
+                            try {
+                                throw task.getException();
+                            } catch (Exception e){
+                                erro = "Erro ao cadastrar";
+                            }
+
+                            Snackbar snac = Snackbar.make(
+                                    v,
+                                    erro,
+                                    Snackbar.LENGTH_SHORT
+                            );
+
+                            snac.setBackgroundTint(Color.BLACK);
+                            snac.setTextColor(Color.WHITE);
+                            snac.show();
+
+                        }
+
+                    }
+                });
+
+    }
+
+    public void voltar(View v){
+
+        Intent intent = new Intent(
+                CadastroActivity.this,
+                MainActivity.class
+        );
+
+        startActivity(intent);
+        finish();
+
+    }
+}
---
+++ app/src/main/java/com/example/financas/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.financas;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email, senha;
+    Button logar, cadastrar;
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        email = findViewById(R.id.edtEmail);
+        senha = findViewById(R.id.edtSenha);
+        logar = findViewById(R.id.btnLogar);
+        cadastrar = findViewById(R.id.btnCadastrar);
+    }
+    public void logar(View v){
+        String mail = email.getText().toString();
+        String password = senha.getText().toString();
+
+        if(mail.isEmpty() || password.isEmpty()){
+            Snackbar snac = Snackbar.make(v, "Preencha todos os campos", Snackbar.LENGTH_SHORT);
+            snac.setBackgroundTint(Color.BLACK);
+            snac.setTextColor(Color.WHITE);
+            snac.show();
+        } else {
+            logarUsuario(v);
+        }
+    }
+    private void logarUsuario(View v) {
+        String mail = email.getText().toString();
+        String password = senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            irTelaPrincipal(v);
+                        } else {
+                            String erro;
+                            try {
+                                throw task.getException();
+                            } catch (Exception e){
+                                erro = "Email ou senha incorretos";
+                            }
+
+                            Snackbar snac = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                            snac.setBackgroundTint(Color.BLACK);
+                            snac.setTextColor(Color.WHITE);
+                            snac.show();
+                        }
+                    }
+                });
+    }
+    private void irTelaPrincipal(View v){
+        Intent i = new Intent(
+                MainActivity.this,
+                HomeActivity.class
+        );
+        startActivity(i);
+        finish();
+    }
+    public void irCadastro(View v){
+        Intent i2 = new Intent(
+                MainActivity.this,
+                CadastroActivity.class
+        );
+        startActivity(i2);
+        finish();
+    }
+}
